db4e756f2a419d301676e8bb5f8bd76cbce080ca
Khairul Hidayat <contact@khairul.my.id>
2019-08-17 20:03:28 +0700

    Fix testing failed

--- __tests__/App-test.js
+++ __tests__/App-test.js
@@ -7,10 +7,6 @@ jest.mock('@react-native-community/async-storage', () =>
   require('@react-native-community/async-storage/jest/async-storage-mock'),
 );
 
-jest.mock('react-native-reanimated', () =>
-  require('react-native-reanimated/mock'),
-);
-
 it('renders correctly', () => {
   renderer.create(<App />);
 });
